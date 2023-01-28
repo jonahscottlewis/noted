2b1f626e96b8b22e00cf3d87e89515919bcf7329
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-01-27 21:15:54 -0800

    added to html and js

---
+++ Nutrition/nut-script.js
@@ -0,0 +1,31 @@
+function getApi() {
+    // fetch request gets a list of all the repos for the node.js organization
+    var requestUrl = 'https://api.spoonacular.com/recipes/autocomplete?number=10&query=chick';
+  
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data)
+        //Loop over the data to generate a table, each table row will have a link to the repo url
+        for (var i = 0; i < data.length; i++) {
+          // Creating elements, tablerow, tabledata, and anchor
+          var createTableRow = document.createElement('tr');
+          var tableData = document.createElement('td');
+          var link = document.createElement('a');
+  
+          // Setting the text of link and the href of the link
+          link.textContent = data[i].html_url;
+          link.href = data[i].html_url;
+  
+          // Appending the link to the tabledata and then appending the tabledata to the tablerow
+          // The tablerow then gets appended to the tablebody
+          tableData.appendChild(link);
+          createTableRow.appendChild(tableData);
+          tableBody.appendChild(createTableRow);
+        }
+      });
+  };
+
+  fetchButton.addEventListener('click', getApi);
