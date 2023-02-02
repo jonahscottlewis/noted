5ab90f39df04fb819614b6e797061e7cc56876f9
Bri Dray <stanggurl02@gmail.com>
2023-02-01 20:08:01 -0800

    Added second part of API to java, added classes to HTML and added a few styles to CSS

--- LandingPage/land-script.js
+++ LandingPage/land-script.js
@@ -1,36 +1,30 @@
 var fetchButton = document.getElementById('fetch-button');
 
 function getApi() {
-    // fetch request gets a list of all 
-    var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=301abb42e89fddd7a250ac02d53fe894';
-  
-    fetch(requestUrl)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log(data)
-        //Loop over the data to generate a table, each table row will have a link to the repo url
-        // for (var i = 0; i < data.length; i++) {
-        //   // Creating elements, tablerow, tabledata, and anchor
-        //   var createTableRow = document.createElement('tr');
-        //   var tableData = document.createElement('td');
-        //   var link = document.createElement('a');
-  
-        //   // Setting the text of link and the href of the link
-        //   link.textContent = data[i].html_url;
-        //   link.href = data[i].html_url;
-  
-        //   // Appending the link to the tabledata and then appending the tabledata to the tablerow
-        //   // The tablerow then gets appended to the tablebody
-        //   tableData.appendChild(link);
-        //   createTableRow.appendChild(tableData);
-        //   tableBody.appendChild(createTableRow);
-        // }
-      });
-  };
+  // fetch request gets a list of all 
+  var requestlatlongUrl = 'http://api.openweathermap.org/geo/1.0/zip?zip=95991&appid=301abb42e89fddd7a250ac02d53fe894';
 
-  fetchButton.addEventListener('click', getApi);
+  fetch(requestlatlongUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      var longitude = data.lon
+      var latitude = data.lat
+      console.log(longitude, latitude)
+      var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=imperial&appid=301abb42e89fddd7a250ac02d53fe894'
+      fetch(requestUrl)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+        })
+    });
+};
+
+fetchButton.addEventListener('click', getApi);
 
 
 
@@ -39,10 +33,10 @@ function getApi() {
 
 
 
-  // -------------------------------modal----------------------------------
-  var myModal = document.getElementById('fetch-button');
+// -------------------------------modal----------------------------------
+var myModal = document.getElementById('fetch-button');
 
-  addnote.addEventListener('click', myModal);
- 
- 
-  $('#myModal').modal(options)
+addnote.addEventListener('click', myModal);
+
+
+$('#myModal').modal(options)
