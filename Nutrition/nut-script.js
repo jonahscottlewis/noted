77e5f13cfcedcdd8ffc5bf8616cabf46ce2b7f82
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-02-04 14:39:23 -0800

    updated nav links and added notes to code

--- Nutrition/nut-script.js
+++ Nutrition/nut-script.js
@@ -2,10 +2,15 @@
 var fetchButton = document.getElementById('fetch-button');
 var addButton = document.getElementById('add');
 var recipeCard = document.getElementById('recipe');
+//var savedList = getElementById('savedList');
+
+// Saving selected recipes in the local storage.
+var savedRecipes = document.getElementById('addRecipe');
+//var savedRecipes = localStorage.getItem();
 
 function getApi() {
 
-
+ // Getting the API url and inserting the user's search to show correlating results.
   var userSearch = document.getElementById('userSearch').value;
   var requestUrl = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=f77fd2f488d240c483a26996e70388ac&number=10&query=' + userSearch + '&instructionsRequired=true&addRecipeInformation=true';
   var urlResults = [];
@@ -19,18 +24,21 @@ function getApi() {
     .then(function (data) {
       console.log(data)
       for (var i = 0; i < data.results.length; i++) {
-        // parsing the information and creating an paragraph with the title and an anchor tag with the url of the results.
+        // Parsing the information and creating an paragraph with the title and an anchor tag with the url of the results.
         var sourceUrl = data.results[i].sourceUrl;
         var recipeTitle = document.createElement('p');
         var recipeLink = document.createElement('a');
-        // making the <a> tags clickable links.
+        //var addButton = document.createElement('button')
+        // Making the <a> tags clickable links.
         recipeLink.setAttribute('href', sourceUrl);
         recipeLink.setAttribute('target', '_blank');
+        // Adding the recipe title to display with the url.
         recipeTitle.innerHTML = data.results[i].title;
         recipeLink.innerHTML = sourceUrl;
         
         recipeCard.appendChild(recipeTitle);
         recipeCard.appendChild(recipeLink);
+        //recipeLink.appendChild(addButton);
 
         recipeLink.classList.add('recipeUrl');
       };
@@ -52,6 +60,12 @@ function addRecipe() {
   }
 }
 
+//addButton.addEventListiner('click', function(){
+//if (){
+ // localStorage.setItem("count", count);
+//}
+//});
+
 //Adding event listener to make buttons display results and add URLs to
 fetchButton.addEventListener('click', getApi);
-addButton.addEventListiner('click', addRecipe);
+
