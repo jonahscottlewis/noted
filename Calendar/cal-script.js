95feab7ae22b2fd427d0ed4ba0d7e673f50b77d9
jonahscottlewis <118423801+jonahscottlewis@users.noreply.github.com>
2023-02-24 15:21:12 -0800

    fixed calendar page

--- Calendar/cal-script.js
+++ Calendar/cal-script.js
@@ -1,161 +1,134 @@
-let currentMonth = 0;
-
-// user selecting the month
-let picked = null;
-
-// checking local storage if 'events' is true (?); if not true(:) then we will get an blankSquare string [] back!;
+let nav = 0;
+let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
 
 const calendar = document.getElementById('calendar');
-const newModal = document.getElementById('newModal');
-const deleteModal = document.getElementById('deleteModal');
-const background = document.getElementById('modal-bg');
-const newEventTitle = document.getElementById('newEventTitle');
-const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const newEventModal = document.getElementById('newEventModal');
+const deleteEventModal = document.getElementById('deleteEventModal');
+const backDrop = document.getElementById('modalBackDrop');
+const eventTitleInput = document.getElementById('eventTitleInput');
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// modal will connect the event created to the date/month/year the user selected;
 function openModal(date) {
-  picked = date;
+  clicked = date;
 
-  const eventForDay = events.find(e => e.date === picked);
+  const eventForDay = events.find(e => e.date === clicked);
 
   if (eventForDay) {
     document.getElementById('eventText').innerText = eventForDay.title;
-    deleteModal.style.display = 'block';
+    deleteEventModal.style.display = 'block';
   } else {
-    newModal.style.display = 'block';
+    newEventModal.style.display = 'block';
   }
 
-  background.style.display = 'block';
+  backDrop.style.display = 'block';
 }
 
+function load() {
+  const dt = new Date();
 
-// tracks in realtime what the current date/month/year is whenever user goes to webpage;
-function refresh() {
-  const newDay = new Date();
-
-  if (currentMonth !== 0) {
-    newDay.setMonth(new Date().getMonth() + currentMonth);
+  if (nav !== 0) {
+    dt.setMonth(new Date().getMonth() + nav);
   }
 
-  const day = newDay.getDate();
-  const month = newDay.getMonth();
-  const year = newDay.getFullYear();
+  const day = dt.getDate();
+  const month = dt.getMonth();
+  const year = dt.getFullYear();
 
-  const firstDayPerMonth = new Date(year, month, 1);
-  const daysEachMonth = new Date(year, month + 1, 0).getDate();
-
-  const dateString = firstDayPerMonth.toLocaleDateString('en-us', {
+  const firstDayOfMonth = new Date(year, month, 1);
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  
+  const dateString = firstDayOfMonth.toLocaleDateString('en-us', {
     weekday: 'long',
     year: 'numeric',
     month: 'numeric',
     day: 'numeric',
   });
+  const paddingDays = weekdays.indexOf(dateString.split(', ')[0]);
 
-  // blankspaces will take up the empty spaces per month to equal 7 days per row;
-  const blankSpaces = weekDays.indexOf(dateString.split(', ')[0]);
-
-  // shows the full spelling of the current month on the webpage;
-  document.getElementById('monthShown').innerText =
-    `${newDay.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
+  document.getElementById('monthDisplay').innerText = 
+    `${dt.toLocaleDateString('en-us', { month: 'long' })} ${year}`;
 
   calendar.innerHTML = '';
 
-  // combining the correct days per month + blank spaces to fill the rest of the space;
-  for (let i = 1; i <= blankSpaces + daysEachMonth; i++) {
+  for(let i = 1; i <= paddingDays + daysInMonth; i++) {
     const daySquare = document.createElement('div');
     daySquare.classList.add('day');
-    
-    const dayString = `${month + 1}/${i - blankSpaces}/${year}`;
 
-    if (i > blankSpaces) {
-      daySquare.innerText = i - blankSpaces;
+    const dayString = `${month + 1}/${i - paddingDays}/${year}`;
 
+    if (i > paddingDays) {
+      daySquare.innerText = i - paddingDays;
       const eventForDay = events.find(e => e.date === dayString);
 
-      // currentDay will be shown in the month via different color scheme from CSS;
-      if (i - blankSpaces === day && currentMonth === 0) {
+      if (i - paddingDays === day && nav === 0) {
         daySquare.id = 'currentDay';
       }
 
-      // everytime user puts in a new event/task into calendar;
       if (eventForDay) {
         const eventDiv = document.createElement('div');
-        eventDiv.classList.add('eventMade');
+        eventDiv.classList.add('event');
         eventDiv.innerText = eventForDay.title;
         daySquare.appendChild(eventDiv);
       }
 
-      // modal will pop up whenver user clicked on a calendar day;
       daySquare.addEventListener('click', () => openModal(dayString));
-
     } else {
-      daySquare.classList.add('square-padding');
+      daySquare.classList.add('padding');
     }
 
-    // puts information into the calendar;
-    calendar.appendChild(daySquare);
+    calendar.appendChild(daySquare);    
   }
 }
 
-// selecting an active event/task then user decides to cancel/close it;
 function closeModal() {
-  newEventTitle.classList.remove('error');
-  newModal.style.display = 'none';
-  deleteModal.style.display = 'none';
-  background.style.display = 'none';
-  newEventTitle.value = '';
-  picked = null;
-  refresh();
+  eventTitleInput.classList.remove('error');
+  newEventModal.style.display = 'none';
+  deleteEventModal.style.display = 'none';
+  backDrop.style.display = 'none';
+  eventTitleInput.value = '';
+  clicked = null;
+  load();
 }
 
-// user saving a new event/task will get an error is there is no text inputted;
-// user completing the text input will have their event/task placed into the date selected;
 function saveEvent() {
-  if (newEventTitle.value) {
-    newEventTitle.classList.remove('error');
+  if (eventTitleInput.value) {
+    eventTitleInput.classList.remove('error');
 
     events.push({
-      date: picked,
-      title: newEventTitle.value,
+      date: clicked,
+      title: eventTitleInput.value,
     });
 
     localStorage.setItem('events', JSON.stringify(events));
     closeModal();
-
   } else {
-    newEventTitle.classList.add('error');
+    eventTitleInput.classList.add('error');
   }
 }
 
-// event/task will disappear from calendar after user decides to delete it;
 function deleteEvent() {
-
-  events = events.filter(e => e.date !== picked);
+  events = events.filter(e => e.date !== clicked);
   localStorage.setItem('events', JSON.stringify(events));
-
   closeModal();
 }
 
-// the next and back buttons focus on changing the months;
-// the last four buttons are listening for user clicking on modal button;
-function allButtons() {
-
-  document.getElementById('next-Btn').addEventListener('click', () => {
-    currentMonth++; refresh();
-
+function initButtons() {
+  document.getElementById('nextButton').addEventListener('click', () => {
+    nav++;
+    load();
   });
 
-  document.getElementById('back-Btn').addEventListener('click', () => {
-    currentMonth--; refresh();
-
+  document.getElementById('backButton').addEventListener('click', () => {
+    nav--;
+    load();
   });
 
-  document.getElementById('save-Btn').addEventListener('click', saveEvent);
-  document.getElementById('cancel-Btn').addEventListener('click', closeModal);
-  document.getElementById('delete-Btn').addEventListener('click', deleteEvent);
-  document.getElementById('close-Btn').addEventListener('click', closeModal);
+  document.getElementById('saveButton').addEventListener('click', saveEvent);
+  document.getElementById('cancelButton').addEventListener('click', closeModal);
+  document.getElementById('deleteButton').addEventListener('click', deleteEvent);
+  document.getElementById('closeButton').addEventListener('click', closeModal);
 }
 
-allButtons();
-refresh();
+initButtons();
+load();
